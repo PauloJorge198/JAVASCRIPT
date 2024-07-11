f3bd1e4dd3d4bbbba2f4ef63fd8c79e306389d4b
PauloJorge <152046210+PauloJorge198@users.noreply.github.com>
2024-07-11 11:24:05 -0300

    atualizacao

---
+++ aula013/ambiente-teste.js
@@ -0,0 +1,22 @@
+//Estrutura mais facilde fazer
+console.log('Vai começar...')
+for (var c = 1 ; c<=10 ; c++){
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')
+
+/* Estrututra de repetição com teste lógico no final.
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c<=6)
+
+
+/* a mais encontrada. Estrututra de repetição com teste lógico no início.
+var c = 1
+while (c<=10){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
---
+++ aula014/ex016/scrip.js
@@ -0,0 +1,8 @@
+function contar(){
+    var txti = document.getElementById('txti') 
+    var txtf = document.getElementById('txtf')
+    var txtp = document.getElementById('txtp')
+    var res = document.getElementById('res')
+    var total = Number(txti.value)-Number(txtf.value)
+    
+}
